54bdbd60cf603c4512de658cb5b5179bdabd090f
krutarth-rao <krutarth.rao@hpe.com>
2021-04-17 20:02:10 -0700

    Project boilerplate added

---
+++ app/main.py
@@ -0,0 +1,8 @@
+"""
+https://github.com/Smattr/needtoknow
+https://ifttt.com/applets/YnbGBZDy
+https://ifttt.com/applets/209884p-add-rss-feed-to-daily-e-mail-digest
+
+https://github.com/randombit/grab-rss
+
+"""
